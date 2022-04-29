32f1f6e5052d67c37a6b7ca0bbac92efca73403b
username <youremail@domain.com>
2022-04-28 22:58:54 -0400

    latest codes

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(function(req, res, next) {
 app.use('/', indexRouter);
 app.use('/teams', teamsRouter);
 app.use('/users', usersRouter);
-app.use('/players', playersRouter);
+app.use('/', playersRouter);
 
 app.use(function(req, res, next) {
   res.locals.user = req.user;
